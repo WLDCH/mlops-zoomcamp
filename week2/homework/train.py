607564153671899c53a7ddb01381d5742b6ccf04
WLDCH <wchrimni@gmail.com>
2022-05-26 17:29:38 +0200

    homework added

---
+++ week2/homework/train.py
@@ -0,0 +1,43 @@
+import argparse
+import os
+import pickle
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+import mlflow
+
+def load_pickle(filename: str):
+    with open(filename, "rb") as f_in:
+        return pickle.load(f_in)
+
+
+
+
+def run(data_path):
+    with mlflow.start_run():
+        mlflow.set_tag('developper', 'walid')
+        mlflow.set_tag('homework', 'yes')
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
+
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_valid)
+
+        rmse = mean_squared_error(y_valid, y_pred, squared=False)
+
+
+if __name__ == '__main__':
+    mlflow.set_experiment('homework')
+
+    mlflow.sklearn.autolog()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data_path",
+        default="./output",
+        help="the location where the processed NYC taxi trip data was saved."
+    )
+    args = parser.parse_args()
+
+    run(args.data_path)
