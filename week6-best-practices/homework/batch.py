d3f0f4c4aed0ad2182700606fd8cb84232270a36
WLDCH <wchrimni@gmail.com>
2022-07-24 20:25:12 +0200

    Refactor with black and isort

--- week6-best-practices/homework/batch.py
+++ week6-best-practices/homework/batch.py
@@ -1,76 +1,67 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import sys
-import pickle
-import pandas as pd
 import os
+import pickle
+import sys
 
+import pandas as pd
 
 S3_ENDPOINT_URL = os.getenv("S3_ENDPOINT_URL", "http://localhost:4566")
 
-with open('model.bin', 'rb') as f_in:
+with open("model.bin", "rb") as f_in:
     dv, lr = pickle.load(f_in)
 
+
 def get_input_path(year, month):
-    default_input_pattern = 's3://nyc-duration/in/{year:04d}-{month:02d}.parquet'
-    input_pattern = os.getenv('INPUT_FILE_PATTERN', default_input_pattern)
+    default_input_pattern = "s3://nyc-duration/in/{year:04d}-{month:02d}.parquet"
+    input_pattern = os.getenv("INPUT_FILE_PATTERN", default_input_pattern)
     return input_pattern.format(year=year, month=month)
 
 
 def get_output_path(year, month):
-    default_output_pattern = 's3://nyc-duration/out/{year:04d}-{month:02d}.parquet'
-    output_pattern = os.getenv('OUTPUT_FILE_PATTERN', default_output_pattern)
+    default_output_pattern = "s3://nyc-duration/out/{year:04d}-{month:02d}.parquet"
+    output_pattern = os.getenv("OUTPUT_FILE_PATTERN", default_output_pattern)
     return output_pattern.format(year=year, month=month)
+
 
 def read_data(input_path):
 
     if S3_ENDPOINT_URL is not None:
-        options = {
-            'client_kwargs': {
-                'endpoint_url': S3_ENDPOINT_URL
-            }
-        }
+        options = {"client_kwargs": {"endpoint_url": S3_ENDPOINT_URL}}
 
         df = pd.read_parquet(input_path, storage_options=options)
     else:
         df = pd.read_parquet(input_path)
 
-    return(df)
+    return df
+
 
 def save_data(data, output_path):
 
     if S3_ENDPOINT_URL is not None:
-        options = {
-            'client_kwargs': {
-                'endpoint_url': S3_ENDPOINT_URL
-            }
-        }
+        options = {"client_kwargs": {"endpoint_url": S3_ENDPOINT_URL}}
 
         data.to_parquet(
             output_path,
-            engine='pyarrow',
+            engine="pyarrow",
             compression=None,
             index=False,
-            storage_options=options
+            storage_options=options,
         )
     else:
-        data.to_parquet(
-            output_path,
-            engine='pyarrow',
-            compression=None,
-            index=False
-        )
+        data.to_parquet(output_path, engine="pyarrow", compression=None, index=False)
+
 
 def prepare_data(df, categorical):
-    
-    df['duration'] = df.dropOff_datetime - df.pickup_datetime
-    df['duration'] = df.duration.dt.total_seconds() / 60
+
+    df["duration"] = df.dropOff_datetime - df.pickup_datetime
+    df["duration"] = df.duration.dt.total_seconds() / 60
 
     df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
 
-    df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
-    
+    df[categorical] = df[categorical].fillna(-1).astype("int").astype("str")
+
     return df
 
 
@@ -78,27 +69,25 @@ def main(year, month):
 
     input_path = get_input_path(year, month)
     df = read_data(input_path)
-    df = prepare_data(df, ['PUlocationID', 'DOlocationID'])
-    df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
+    df = prepare_data(df, ["PUlocationID", "DOlocationID"])
+    df["ride_id"] = f"{year:04d}/{month:02d}_" + df.index.astype("str")
 
-
-    dicts = df[['PUlocationID', 'DOlocationID']].to_dict(orient='records')
+    dicts = df[["PUlocationID", "DOlocationID"]].to_dict(orient="records")
     X_val = dv.transform(dicts)
     y_pred = lr.predict(X_val)
 
-
-    print('predicted mean duration:', y_pred.mean())
-
+    print("predicted mean duration:", y_pred.mean())
 
     df_result = pd.DataFrame()
-    df_result['ride_id'] = df['ride_id']
-    df_result['predicted_duration'] = y_pred
+    df_result["ride_id"] = df["ride_id"]
+    df_result["predicted_duration"] = y_pred
 
-    output_path= get_output_path(year, month)
+    output_path = get_output_path(year, month)
     save_data(df, output_path)
 
+
 if __name__ == "__main__":
-    
+
     year = int(sys.argv[1])
     month = int(sys.argv[2])
     main(year, month)
--- week6-best-practices/homework/tests/test_batch.py
+++ week6-best-practices/homework/tests/test_batch.py
@@ -1,9 +1,11 @@
-import batch
-import pandas as pd
+from datetime import datetime
+
 import numpy as np
+import pandas as pd
 from deepdiff import DeepDiff
 
-from datetime import datetime
+import batch
+
 
 def dt(hour, minute, second=0):
     return datetime(2021, 1, 1, hour, minute, second)
@@ -13,22 +15,30 @@ data = [
     (None, None, dt(1, 2), dt(1, 10)),
     (1, 1, dt(1, 2), dt(1, 10)),
     (1, 1, dt(1, 2, 0), dt(1, 2, 50)),
-    (1, 1, dt(1, 2, 0), dt(2, 2, 1)),        
+    (1, 1, dt(1, 2, 0), dt(2, 2, 1)),
 ]
 
-columns = ['PUlocationID', 'DOlocationID', 'pickup_datetime', 'dropOff_datetime']
+columns = ["PUlocationID", "DOlocationID", "pickup_datetime", "dropOff_datetime"]
 df = pd.DataFrame(data, columns=columns)
 
-def test_prepare_data():
-    
-    actual_output = batch.prepare_data(df, ['PUlocationID', 'DOlocationID'])
-    expected_output = pd.DataFrame(columns = columns + ['duration'], data=[
-        ("-1", "-1", dt(1, 2), dt(1, 10), 8.0),
-        ("1", "1", dt(1, 2), dt(1, 10), 8.0),
-    ])
 
-    diff = DeepDiff(actual_output.to_dict('records'), expected_output.to_dict('records'), significant_digits=1)
-    print(f'diff={diff}')
-    
-    assert 'type_changes' not in diff
-    assert 'values_changed' not in diff
+def test_prepare_data():
+
+    actual_output = batch.prepare_data(df, ["PUlocationID", "DOlocationID"])
+    expected_output = pd.DataFrame(
+        columns=columns + ["duration"],
+        data=[
+            ("-1", "-1", dt(1, 2), dt(1, 10), 8.0),
+            ("1", "1", dt(1, 2), dt(1, 10), 8.0),
+        ],
+    )
+
+    diff = DeepDiff(
+        actual_output.to_dict("records"),
+        expected_output.to_dict("records"),
+        significant_digits=1,
+    )
+    print(f"diff={diff}")
+
+    assert "type_changes" not in diff
+    assert "values_changed" not in diff
